b8be5b0e49e68e9816edfd386889c959f25efa23
Kim <kim@argomall.onmicrosoft.com>
2023-03-07 09:49:10 +0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
+import { ProductsComponent } from './components/products/products.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchbarComponent,
+    ProductsComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
